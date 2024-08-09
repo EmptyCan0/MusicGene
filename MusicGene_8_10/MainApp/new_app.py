35bf29e703b0d836c0cae3c8ea43574d762e9d19
emptycan01 <asadarikuto1004@outlook.jp>
2024-08-09 23:49:23 +0900

    8_10

---
+++ MusicGene_8_10/MainApp/new_app.py
@@ -0,0 +1,413 @@
+from flask import Flask, request, render_template, redirect, url_for, jsonify, send_from_directory, send_file
+from pydub import AudioSegment
+import os
+import threading
+
+import librosa
+import soundfile as sf
+import numpy as np
+import io
+
+import re
+from pydub.silence import detect_nonsilent
+
+#グローバル変数の定義
+Music_Genre = ""
+Music_Name = ""
+Music_Path = ""
+Basic_Sound = None
+NowScale = 1
+MaxCount = 1
+Count = 0
+ProgressNumber = 0
+Duration = 120000
+
+sent = False
+
+SoundPitch = 39
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+UPLOAD_FOLDER = 'uploads'
+@app.route('/uploads/<filename>')
+def get_file(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename)
+
+################JSとのデータやり取り################
+@app.route('/submit', methods=['POST'])
+def WhenMusicGenreSelected():
+    data = request.get_json()
+    selected_value = data['selectedValue']
+    global Music_Genre 
+    Music_Genre = str(selected_value)
+    # 選択された値に対してPythonコードを実行
+    print(f"選択された値: {selected_value}")
+    # ここにPythonで処理を追加
+    response = {
+        'response': f'受け取った値は: {selected_value}'
+    }
+    return jsonify(response)
+
+#曲名を選んだあと
+@app.route('/submit2', methods=['POST'])
+def WhenMusicNameSelected():
+    data = request.get_json()
+    newpath = data['path']
+    response = {
+        'response': f'受け取った値は: {newpath}'
+    }
+    print(newpath)
+    filename = str(newpath)
+    SerchMaxCount(filename)
+    return jsonify(response)
+
+def SerchMaxCount(filename):
+    global Music_Path
+    Music_Path = filename
+    #MaxScaleの計測
+    count = 0
+    with open(Music_Path, 'rt', encoding="utf-8") as f:
+        text1 = f.readlines()
+        for t in text1:
+            count += 1
+    f.close()
+    global MaxCount
+    MaxCount = count
+    print(MaxCount)
+
+#ギアの値（ピッチ)を取得
+@app.route('/update_gear', methods=['POST'])
+def update_gear():
+    gear_value = request.json.get('gearValue')
+    # ここでギアの値を使用して必要な処理を行います
+    global SoundPitch
+    SoundPitch = 39 - gear_value
+    #print(SoundPitch)
+    return jsonify({"status": "success", "gearValue": gear_value})
+################JSとのデータやり取り################
+
+
+##################################################
+############### 音声の生成系の部分 ################
+##################################################
+# 配列をある数の要素ごとに平均値を計算
+def average_block(arr, block_size):
+    #100個の要素数の場合、reshape(-1, 5)により、形状が (20, 5) になる
+    return arr.reshape(-1, block_size).mean(axis=1) #axix=1によって上の20の要素（列）を取得
+
+
+#閾値よりも小さく、それが重複している部分を削除する
+def remove_below_threshold_duplicates(amp_array,time_array, threshold):
+    # 空のリストを初期化
+    amp = []
+    time = []
+    core_time = []
+    switch = False
+
+    # 配列をループして、閾値より低いかつ重複を消去
+    for i in range(len(amp_array)):
+        #一回目だったらスキップ
+        if not amp:
+            amp.append(amp_array[i])
+            time.append(time_array[i])
+            continue
+        
+        if amp[-1] >= threshold:
+            amp.append(amp_array[i])
+            time.append(time_array[i])
+        elif amp_array[i] >= threshold:
+            amp.append(amp_array[i])
+            time.append(time_array[i])
+            if not switch:
+                core_time.append(time_array[i])
+                switch = True
+            core_time[-1] = (core_time[-1] + time_array[i]) / 2
+            switch = False
+        else:
+            time[-1] = (time[-1] + time_array[i]) / 2
+            if not switch:
+                core_time.append(time_array[i])
+                switch = True
+            else:
+                core_time[-1] = (core_time[-1] + time_array[i]) / 2
+    # リストをNumPy配列に変換
+    return np.array(amp),np.array(time),np.array(core_time)
+
+
+#求めた時間のリストから、実際に音声ファイルを分割する
+def split_wav_by_ranges(audio_data, peak_times):
+    audio_seg = AudioSegment.from_wav(audio_data)
+    # 音声の総時間を秒で取得
+    total_duration_sec = len(audio_seg)/1000
+    
+    ranges = []
+    min_range = 0.15
+    for i in range(0,len(peak_times)):
+        if i == 0:
+            continue
+        if peak_times[i] - peak_times[i - 1] > min_range:
+            ranges.append([peak_times[i - 1],peak_times[i]])
+    
+
+    cut_second = 0.1
+    if total_duration_sec - ranges[-1][1] > cut_second:
+        ranges.append([ranges[-1][1],total_duration_sec])
+
+    sounds = []
+    
+    #   分割された音声ファイルを保存する
+    for i, (start_sec, end_sec) in enumerate(ranges):
+        # 秒単位をミリ秒単位に変換
+        start_time = start_sec * 1000  # ミリ秒単位
+        end_time = end_sec * 1000  # ミリ秒単位
+        
+        # 指定した時間範囲で分割
+        split_audio = audio_seg[start_time:end_time]
+        
+        # 分割した音声を新しいファイルとして保存
+        sounds.append(split_audio)
+        
+    return sounds
+
+
+def EditMusicFile(audio_data):
+    # バイナリデータをBytesIOオブジェクトに変換
+    audio = audio_data.read()
+    audio_file = io.BytesIO(audio)
+
+    # BytesIOオブジェクトをlibrosaで読み込むためにsoundfileを使用
+    y, sr = sf.read(audio_file, dtype='float32')
+
+    # 必要に応じてlibrosaで使用可能な形式に変換（リサンプリング）
+    target_sr = 22050
+    y = librosa.resample(y.T, orig_sr=sr, target_sr=target_sr)
+
+    # サンプル値の絶対値を取得
+    reduce_number = 50
+    y_reduced = np.abs(y[::reduce_number])
+    block_number = 10
+    y_reduced = y_reduced[:((len(y_reduced)//block_number) * block_number)]
+
+    y_average = average_block(y_reduced,block_number)
+    time = reduce_number * block_number * np.arange(len(y_average)) / sr 
+
+    new_y , new_time, peak_time= remove_below_threshold_duplicates(y_average,time,threshold=np.mean(y_average))
+    
+    if len(peak_time) == 0:
+        return None
+
+    #＝＝＝＝＝＝＝＝＝＝＝＝＝音声を分割している重要な部分＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+    soundfiles = split_wav_by_ranges(audio_data, peak_times = peak_time)
+    #＝＝＝＝＝＝＝＝＝＝＝＝＝音声を分割している重要な部分＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+    return soundfiles
+
+    
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({"error": "No file part in the request"}), 400
+
+    file = request.files['file']
+    
+    if file.filename == '':
+        return jsonify({"error": "No file selected"}), 400
+    global sent
+    if sent:
+        return jsonify({"error": "FUCK YOU"}), 400
+
+    if file:
+        sent = True
+        global Basic_Sound
+        Basic_Sound = file
+        # 音声ファイルの編集
+        edited_file = generate_music(file)
+
+        # AudioSegmentをバイナリデータに変換
+        audio_io = io.BytesIO()
+        edited_file.export(audio_io, format="wav")
+        audio_io.seek(0)
+
+        return send_file(audio_io, mimetype='audio/wav', as_attachment=True, download_name='audio.wav')
+
+
+@app.route('/regenerate', methods=['POST'])
+def Regenerate():
+    if 'file' not in request.files:
+        return jsonify({"error": "No file part in the request"}), 400
+
+    file = request.files['file']
+    
+    if file.filename == '':
+        return jsonify({"error": "No file selected"}), 400
+    global sent
+    if sent:
+        return jsonify({"error": "FUCK YOU"}), 400
+    
+    if file:
+        sent = True
+        global Basic_Sound
+        Basic_Sound = file
+        # 音声ファイルの編集
+        edited_file = generate_music(file)
+
+        # AudioSegmentをバイナリデータに変換
+        audio_io = io.BytesIO()
+        edited_file.export(audio_io, format="wav")
+        audio_io.seek(0)
+
+        return send_file(audio_io, mimetype='audio/wav', as_attachment=True, download_name='audio.wav')
+
+@app.route('/generate', methods=['POST'])
+def Generate():
+    if 'file' not in request.files:
+        return jsonify({"error": "No file part in the request"}), 400
+
+    file = request.files['file']
+    
+    if file.filename == '':
+        return jsonify({"error": "No file selected"}), 400
+    
+    global sent
+
+    if sent:
+        return jsonify({"error": "FUCK YOU"}), 400
+    
+
+    global Music_Path
+    newpath = Music_Path.replace("MusicSample","MusicLarge")
+    SerchMaxCount(newpath)
+
+    if file:
+        sent = True
+        global Basic_Sound
+        Basic_Sound = file
+        # 音声ファイルの編集
+        edited_file = generate_music(file)
+
+        # AudioSegmentをバイナリデータに変換
+        audio_io = io.BytesIO()
+        edited_file.export(audio_io, format="wav")
+        audio_io.seek(0)
+
+        return send_file(audio_io, mimetype='audio/wav', as_attachment=True, download_name='audio.wav')
+
+
+def EditSoundFile(StartFrame,scale,combined_sound,add_sound):
+    global NowScale
+    start = int(StartFrame)
+    #print(strSoundPath,start)
+    combined_sound = combined_sound.overlay(add_sound, position= 1000 * start / 60)
+    return combined_sound
+   
+def change_pitch(scale,sound):
+    # 音声ファイルを読み込む
+    audio = sound
+    # ピッチを変更する
+    global SoundPitch
+    octaves = (int(scale) - int(SoundPitch)) / 24 #24が違和感もっともない
+    new_sample_rate = int(audio.frame_rate * (4 ** octaves))
+    audio = audio._spawn(audio.raw_data, overrides={
+        "frame_rate": new_sample_rate
+    }).set_frame_rate(audio.frame_rate)
+    
+    #ピッチを変更したaudioファイルを返す
+    return audio
+
+# 各AudioSegmentの最大音量を取得してソートする関数
+def get_max_volume(audio_segment):
+    return audio_segment.max_dBFS
+
+
+def generate_music(file):
+    print("waiting")
+    more_than_2_sounds = False
+    
+    sounds = EditMusicFile(file)
+    if sounds != None:
+        sorted_audio_segments = sorted(sounds, key=get_max_volume, reverse=True)
+        more_than_2_sounds = True
+        sorted_audio_segments[0].export("1.wav", format="wav")
+        sorted_audio_segments[1].export("2.wav", format="wav")
+    else:
+        sounds =AudioSegment.from_wav(file)
+        
+    global Music_Path
+    file_name = Music_Path
+
+    global NowScale
+    NowScale = 0
+    combined_sound = AudioSegment.silent(duration=12000)
+    IsSample = False
+    
+    global Count
+    Count = 0
+    
+    newaudio = None
+
+    with open(file_name, 'rt', encoding="utf-8") as f1:
+        text1 = f1.readlines()
+        for t in text1:
+            if Count == 0:
+                Duration = int(t.split('_')[1])
+                if "MusicSample" in Music_Path:
+                    combined_sound = AudioSegment.silent(duration=12000)
+                    IsSample = True
+                else:
+                    combined_sound = AudioSegment.silent(duration=Duration)
+                    IsSample = False
+                Count += 1
+                continue
+            sentence = t.split(",") 
+            frame = sentence[0]
+            scale = re.search(r'\d+', sentence[1]).group()
+            if int(NowScale) != int(scale):
+                if more_than_2_sounds:
+                    if 'g' in sentence[1]:
+                        newaudio = change_pitch(scale,sorted_audio_segments[0])
+                    else:
+                        newaudio = change_pitch(scale,sorted_audio_segments[1])
+                else:
+                    newaudio = change_pitch(scale,sounds)
+                NowScale = scale
+            combined_sound = EditSoundFile(frame,scale,combined_sound,newaudio)
+            Count += 1
+            #print(Count)
+    f1.close()
+    global SoundPitch
+    print("exporting")
+    combined_sound.export("unko.wav", format="wav")
+
+    if IsSample:
+        outputpath = os.path.join(app.config['UPLOAD_FOLDER'], 'edited_' + str(int(SoundPitch) -39) +  '_' + os.path.basename(Music_Path).split(".txt")[0] + '_sample.wav')
+    else:
+        outputpath = os.path.join(app.config['UPLOAD_FOLDER'], 'edited_' + str(int(SoundPitch) -39) + '_' + os.path.basename(Music_Path).split(".txt")[0] + '.wav')
+    combined_sound.export(outputpath, format="wav")
+    print("returned")
+
+    global sent
+    sent = False
+
+    return combined_sound
+    
+@app.route('/get_ProgressNumber', methods=['GET'])
+def GiveProgressNumber():
+    global NowScale
+    global MaxCount
+    global ProgressNumber
+    global Count
+    ProgressNumber = int(int(Count)/int(MaxCount) * 100)
+    return jsonify({"ProgressNumber": ProgressNumber})
+##################################################
+############### 音声の生成系の部分 ################
+##################################################
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
---
+++ MusicGene_8_10/MainApp/static/script.js
@@ -0,0 +1,312 @@
+//アップロードボタンが押されたとき
+document.addEventListener('DOMContentLoaded', function() {
+    const uploadForm = document.getElementById('uploadForm');
+    const fileInput = document.getElementById('audioFile');
+    var sent  = false;
+
+    uploadForm.addEventListener('submit', function(event) {
+        if (!sent){
+            event.preventDefault();
+            const file = fileInput.files[0];
+            if (file) {
+                sent = true;
+                const formData = new FormData();
+                formData.append('file', file);
+
+                fetch('/upload', {
+                    method: 'POST',
+                    body: formData
+                })
+                .then(response => response.blob())
+                .then(blob => {
+                    const url = URL.createObjectURL(blob);
+                    const audioPlayer = document.getElementById('sample-music');
+                    audioPlayer.src = url;
+                    sent = false;
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+            } else {
+                console.error('No file selected');
+            }
+        }
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+//再度音声を生成
+    var sent = false;
+    document.getElementById('regene').onclick =  async function() {
+        ApploadButtonPushed();
+        const fileInput = document.getElementById('audioFile');
+        const file = fileInput.files[0];
+        if (file) {
+            sent = true;
+            const formData = new FormData();
+            formData.append('file', file);
+
+            fetch('/regenerate', {
+                method: 'POST',
+                body: formData
+            })
+            .then(response => response.blob())
+            .then(blob => {
+                const url = URL.createObjectURL(blob);
+                const audioPlayer = document.getElementById('sample-music');
+                audioPlayer.src = url;
+                sent = false;
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+        } else {
+            console.error('No file selected');
+        }
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var sent = false;
+//本番の音声を生成
+    document.getElementById('gene').onclick =  async function() {
+        GenerateButtonPushed();
+        const fileInput = document.getElementById('audioFile');
+        const file = fileInput.files[0];
+        if (file) {
+            sent = true;
+            const formData = new FormData();
+            formData.append('file', file);
+
+            fetch('/generate', {
+                method: 'POST',
+                body: formData
+            })
+            .then(response => response.blob())
+            .then(blob => {
+                const url = URL.createObjectURL(blob);
+                const audioPlayer = document.getElementById('all-music');
+                audioPlayer.src = url;
+                sent = false;
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+        } else {
+            console.error('No file selected');
+        }
+    }
+});
+
+function WhenMusicNameSelected() {
+    var selectBox1 = document.getElementById("Music_name");
+    console.log(selectBox1.selectedIndex)
+    var MusicName = selectBox1.options[selectBox1.selectedIndex].value;
+    var selectBox2 = document.getElementById("Music_genre");
+    var MusicGenre = selectBox2.options[selectBox2.selectedIndex].value;
+
+    path =  filename = "static//MusicSample//" + String(MusicGenre) + "//" + String(MusicName) + ".txt"
+
+    fetch('/submit2', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ path: path })
+    })
+    .then(response => response.json())
+    .then(data => {
+    });
+}
+
+//リストボックスの中身を変更(初期)
+document.addEventListener('DOMContentLoaded', function() {
+    const listbox_music_genre = document.getElementById('Music_genre');
+
+    // 既存のオプションをクリア
+    listbox_music_genre.innerHTML = '';
+
+    // 新しいオプションを追加
+    const genre_options = [
+        { value: '東方Project', text: '東方Project' },
+        { value: 'FFシリーズ', text: 'FFシリーズ' },
+        { value: '3', text: 'Option 3' }
+    ];
+
+    genre_options.forEach(option => {
+        const new_option = document.createElement('option');
+        new_option.value = option.value;
+        new_option.textContent = option.text;
+        listbox_music_genre.appendChild(new_option);
+    });
+
+    update_musicname();
+    
+});
+
+function update_musicname(){
+    const listbox_music_genre = document.getElementById('Music_genre');
+    const listbox_music_name = document.getElementById('Music_name');
+    const select_genre = listbox_music_genre.value;
+    let music_name_option = [];
+    if (select_genre == '東方Project'){
+        music_name_option = [
+            { value: "ナイトオブナイツ", text: "ナイトオブナイツ"},
+            { value: "最終鬼畜妹フランドール・S", text: "最終鬼畜妹フランドール・S"},
+            { value: "亡き王女の為のセプテット", text: "亡き王女の為のセプテット"},
+            { value: "ネイティブフェイス", text: "ネイティブフェイス"},
+            { value: "恋色マスタースパーク", text: "恋色マスタースパーク"},
+            { value: "いざ、倒れ逝くその時まで", text: "いざ、倒れ逝くその時まで"},
+            { value: "今宵は飄逸なエゴイスト", text: "今宵は飄逸なエゴイスト"}
+        ];
+    }
+    else if (select_genre == 'FFシリーズ'){
+        music_name_option = [
+            { value: "ビッグブリッジの死闘", text: "ビッグブリッジの死闘"},
+        ];
+    }
+    //リストボックスの更新
+    listbox_music_name.innerHTML = '';
+    music_name_option.forEach(option => {
+        const new_option = document.createElement('option');
+        new_option.value = option.value;
+        new_option.textContent = option.text;
+        listbox_music_name.appendChild(new_option);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const listbox_music_genre = document.getElementById('Music_genre');
+    const listbox_music_name = document.getElementById('Music_name');
+
+    listbox_music_genre.addEventListener('change', function(){
+        update_musicname();
+    });
+});
+
+//-----------------------------------------------------------//
+//------------モーデルの操作----------------------------------//
+//-----------------------------------------------------------//
+//閉じるボタン
+function CloseModal(modalID){
+    var modal = document.getElementById(modalID);
+    modal.style.display = "none";
+}
+//モーダルを開くボタン
+function OpenModal(modalID){
+    var modal = document.getElementById(modalID);
+    modal.style.display = "block";
+}
+
+//プログレスバーの実装
+
+function ApploadButtonPushed(){
+    WhenMusicNameSelected();
+
+    var Loading_var = document.querySelector(".loading_progress");
+    var modal = document.getElementById('Modal_wait');
+    modal.style.display = "block";
+    CloseModal('SampleCheck')
+    var width = 0;
+    var interval = setInterval(function(){
+        if (width >= 100){
+            clearInterval(interval);
+            modal.style.display = "none"
+        }else{
+            $.ajax({
+                url: '/get_ProgressNumber',
+                type: 'GET',
+                success: function(response) {
+                    // サーバーからのレスポンスを処理
+                    width = response.ProgressNumber;
+                    Loading_var.style.width = width + '%';
+                    Loading_var.innerHTML = width + '%';
+
+                    // 完了時にモーダルを閉じる
+                    if (width >= 100) {
+                        clearInterval(interval);
+                        modal.style.display = "none";
+                        After_SampleMusicCreated();
+                    }
+                },
+                error: function(error) {
+                    console.log('エラー: ', error);
+                }
+            });
+        }
+    },500); 
+}
+
+
+function GenerateButtonPushed(){
+    var Loading_var = document.querySelector(".loading_progress");
+    var modal = document.getElementById('Modal_wait');
+    modal.style.display = "block";
+    CloseModal('SampleCheck')
+    var width = 0;
+    var interval = setInterval(function(){
+        if (width >= 100){
+            clearInterval(interval);
+            modal.style.display = "none"
+        }else{
+            $.ajax({
+                url: '/get_ProgressNumber',
+                type: 'GET',
+                success: function(response) {
+                    // サーバーからのレスポンスを処理
+                    width = response.ProgressNumber;
+                    Loading_var.style.width = width + '%';
+                    Loading_var.innerHTML = width + '%';
+
+                    // 完了時にモーダルを閉じる
+                    if (width >= 100) {
+                        clearInterval(interval);
+                        modal.style.display = "none";
+                        After_AllMusicCreated();
+                    }
+                },
+                error: function(error) {
+                    console.log('エラー: ', error);
+                }
+            });
+        }
+    },500); 
+}
+
+//サンプルが生成し終わった後
+function After_SampleMusicCreated(){
+    var checkmodal = document.getElementById("SampleCheck");
+    checkmodal.style.display = "block";
+}
+
+//本番局が生成し終わった後
+function After_AllMusicCreated(){
+    var checkmodal = document.getElementById("MusicCheck");
+    checkmodal.style.display = "block";
+}
+
+//スライダーの実装
+function updateGear(value) {
+    document.getElementById('gearValue').textContent = value;
+    // ここでギアの値を使って変数を調整します
+    var gearValue = parseInt(value, 10);
+    // ギアの値を送信
+    $.ajax({
+        url: '/update_gear',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({ gearValue: gearValue }),
+        success: function(response) {
+            console.log("Server response:", response);
+        },
+        error: function(error) {
+            console.error("Error:", error);
+        }
+    });
+}
+
+
+//-----------------------------------------------------------//
+//------------モールドの操作----------------------------------//
+//-----------------------------------------------------------//
